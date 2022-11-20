e2295d22db3a1af4657749b58471af97bb4d44fe
jamextech1 <okorojameschizaram@gmail.com>
2022-11-20 12:59:16 +0100

    completed the mobile nav responsive bar

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ faqOne.forEach((faq) => {
     faq.classList.toggle("faq-icon-rotate-up");
   });
 });
+
+//
+// MOBILE NAV FUNCTION PROGRAMMING
+const btn = document.querySelector(".hamburger");
+const btn2 = document.querySelector(".hamburger-close");
+const btn3 = document.querySelector(".nav-links");
+btn.addEventListener("click", function (e) {
+  btn3.style.top = "0";
+});
+btn2.addEventListener("click", function (e) {
+  btn3.style.top = "-300%";
+});
