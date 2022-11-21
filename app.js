0ae71076a60a44c447f27edcec131b3b28c1b7e7
jamextech1 <okorojameschizaram@gmail.com>
2022-11-21 08:03:14 +0100

    i\ve completed the project even added the back to top btn, about to deploy to netlify

--- app.js
+++ app.js
@@ -12,9 +12,20 @@ faqOne.forEach((faq) => {
 const btn = document.querySelector(".hamburger");
 const btn2 = document.querySelector(".hamburger-close");
 const btn3 = document.querySelector(".nav-links");
+const btn4 = document.querySelectorAll(".nav-links a");
 btn.addEventListener("click", function (e) {
   btn3.style.top = "0";
 });
 btn2.addEventListener("click", function (e) {
   btn3.style.top = "-300%";
 });
+// making the links to go back up onclick
+btn4.forEach((links) => {
+  links.addEventListener("click", function (e) {
+    btn3.style.top = "-300%";
+  });
+});
+// scroll to top btn function
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
